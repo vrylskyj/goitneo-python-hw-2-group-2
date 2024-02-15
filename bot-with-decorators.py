d4fe08f729c65b510b491dc4daf7bfe61e5751a2
Rylskyj Viktor <viktor.r@scopicsoftware.com>
2024-02-15 17:52:37 +0200

    first commit

---
+++ bot-with-decorators.py
@@ -0,0 +1,74 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "Contact not found"
+        except IndexError:
+            return "Invalid command. Please enter a valid command."
+    return inner
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
+
+@input_error
+def add_contact(args, contacts):
+    name, phone = args
+    contacts[name] = phone
+    return "Contact added"
+
+@input_error
+def change_contact(args, contacts):
+    name, new_phone = args
+    if name in contacts:
+        contacts[name] = new_phone
+        return "Contact updated"
+    else:
+        raise KeyError
+    
+@input_error
+def show_phone(args, contacts):
+    name = args[0]
+    if name in contacts:
+        return contacts[name]
+    else:
+        raise KeyError
+
+  
+def show_all(contacts):
+    if not contacts:
+        return "There are no contacts."
+    else:
+        for name, phone in contacts.items():
+            print(f"{name}: {phone}")
+
+
+def main():
+    contacts = {}
+    print("Welcome to the assistant bot!")
+    while True:
+        user_input = input("Enter a command: ")
+        command, *args = parse_input(user_input)
+
+        if command in ["close", "exit"]:
+            print("Good bye!")
+            break
+        elif command == "hello":
+            print("How can I help you?")
+        elif command == "add":
+            print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(show_phone(args, contacts))
+        elif command == "all":
+            show_all(contacts)       
+        else:
+            print("Invalid command.")
+
+if __name__ == "__main__":
+    main()
